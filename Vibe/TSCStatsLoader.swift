7e7a4698fc5dd83ecf856862e821b0a6a8fe2e3a
Thomas Schoffelen <thomas@scholica.com>
2017-02-11 13:02:05 +0100

    Updated project to Swift3 syntax, fixed a few warnings.

--- Vibe/TSCStatsLoader.swift
+++ Vibe/TSCStatsLoader.swift
@@ -16,82 +16,79 @@ class TSCStatsLoader {
         var error: NSDictionary?
         
         if let scriptObject = NSAppleScript(source: myAppleScript) {
-            if let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(
-                &error) {
-                let dates = output.stringValue!.componentsSeparatedByString("\r");
-                var countsDays:[String:Int] = [:]
-                var countsWeeks:[String:Int] = [:]
-                var countsMonths:[String:Int] = [:]
-                var countsDayOfWeek:[String:Int] = [:]
-                
-                var countIncomplete = 0
-                var countComplete = 0
-                
-                let dateFormatter = NSDateFormatter()
-                dateFormatter.locale = NSLocale.currentLocale()
-                dateFormatter.dateStyle = .FullStyle
-                dateFormatter.timeStyle = .MediumStyle
-                
-                let dateFormatter2 = NSDateFormatter()
-                dateFormatter2.dateFormat = "MMM"
-                
-                let weekdayNames = ["", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-                
-                let calendar = NSCalendar.currentCalendar()
+            let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(&error)
+            let dates = output.stringValue!.components(separatedBy: "\r");
+            var countsDays:[String:Int] = [:]
+            var countsWeeks:[String:Int] = [:]
+            var countsMonths:[String:Int] = [:]
+            var countsDayOfWeek:[String:Int] = [:]
             
-                let now = calendar.components([.WeekOfYear, .Day, .Month, .Year], fromDate: NSDate())
-                let nowTime = NSDate().timeIntervalSince1970;
-                
-                var sortableTimes : [Double] = []
-                
-                for item in dates {
-                    if item == "missing value" {
-                        countIncomplete += 1;
-                        continue;
-                    }
-                    
-                    if let date = dateFormatter.dateFromString(item) {
-                        countComplete += 1;
-                        sortableTimes.append(Double(date.timeIntervalSince1970))
-                    }
-                }
-                
-                sortableTimes.sortInPlace()
-                
-                for thenTime in sortableTimes {
-                    let date = NSDate(timeIntervalSince1970: thenTime)
-                    let then = calendar.components([.WeekOfYear, .Weekday, .Day, .Month, .Year], fromDate: date)
-                    
-                    if then.year == now.year {
-                        let weekName = "Week \(then.weekOfYear)"
-                        countsWeeks[weekName] = (countsWeeks[weekName] ?? 0) + 1
-                        
-                        let monthName = "\(then.month)|\(dateFormatter2.stringFromDate(date))"
-                        countsMonths[monthName] = (countsMonths[monthName] ?? 0) + 1
-                    }
-                    
-                    if thenTime >= nowTime - 7 * 86400 {
-                        let dayName = "\(then.year)\(then.month)\(then.day)|\(then.day)-\(then.month)"
-                        countsDays[dayName] = (countsDays[dayName] ?? 0) + 1
-                    }
-                    
-                    let dayOfWeekName = "\(then.weekday)|\(weekdayNames[then.weekday])"
-                    countsDayOfWeek[dayOfWeekName] = (countsDayOfWeek[dayOfWeekName] ?? 0) + 1
-                }
-                
-                return [
-                    "days": countsDays,
-                    "weeks": countsWeeks,
-                    "months": countsMonths,
-                    "weekdays": countsDayOfWeek,
-                    "totals": [
-                        "complete": countComplete,
-                        "incomplete": countIncomplete
-                    ]
-                ];
-            }
-        }
+            var countIncomplete = 0
+            var countComplete = 0
+            
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = Locale.current
+            dateFormatter.dateStyle = .full
+            dateFormatter.timeStyle = .medium
+            
+            let dateFormatter2 = DateFormatter()
+            dateFormatter2.dateFormat = "MMM"
+            
+            let weekdayNames = ["", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+            
+            let calendar = Calendar.current
         
+            let now = (calendar as NSCalendar).components([.weekOfYear, .day, .month, .year], from: Date())
+            let nowTime = Date().timeIntervalSince1970;
+            
+            var sortableTimes : [Double] = []
+            
+            for item in dates {
+                if item == "missing value" {
+                    countIncomplete += 1;
+                    continue;
+                }
+                
+                if let date = dateFormatter.date(from: item) {
+                    countComplete += 1;
+                    sortableTimes.append(Double(date.timeIntervalSince1970))
+                }
+            }
+            
+            sortableTimes.sort()
+            
+            for thenTime in sortableTimes {
+                let date = Date(timeIntervalSince1970: thenTime)
+                let then = (calendar as NSCalendar).components([.weekOfYear, .weekday, .day, .month, .year], from: date)
+                
+                if then.year == now.year {
+                    let weekName = "Week \(then.weekOfYear)"
+                    countsWeeks[weekName] = (countsWeeks[weekName] ?? 0) + 1
+                    
+                    let monthName = "\(then.month)|\(dateFormatter2.string(from: date))"
+                    countsMonths[monthName] = (countsMonths[monthName] ?? 0) + 1
+                }
+                
+                if thenTime >= nowTime - 7 * 86400 {
+                    let dayName = "\(then.year)\(then.month)\(then.day)|\(then.day)-\(then.month)"
+                    countsDays[dayName] = (countsDays[dayName] ?? 0) + 1
+                }
+                
+                let dayOfWeekName = "\(then.weekday)|\(weekdayNames[then.weekday!])"
+                countsDayOfWeek[dayOfWeekName] = (countsDayOfWeek[dayOfWeekName] ?? 0) + 1
+            }
+            
+            return [
+                "days": countsDays,
+                "weeks": countsWeeks,
+                "months": countsMonths,
+                "weekdays": countsDayOfWeek,
+                "totals": [
+                    "complete": countComplete,
+                    "incomplete": countIncomplete
+                ]
+            ];
+        }
         
         return [:];
     }
